5c6ee3276b43aefe20038fe16b94e5d837eec4ed
sonukr <sonuyadav9756@gmail.com>
2017-01-05 00:25:45 +0530

    map and image updated and marker dragable

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var app = angular.module("Gflick", ['uiGmapgoogle-maps']);
 
 
 
-app.controller("flicker", function($http,$scope,$scope, $log, $timeout){
-    var page = '1';
+app.controller("flicker", function($http,$scope,$scope, $log, $timeout,$anchorScroll,$location){
+    var page = '10';
     var base = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=';
     var api_key = '2d18b4b58397e0f73968ee0755a79a5c';
     var auth_token = '72157677278492840-779808da05e88571';
@@ -16,50 +16,61 @@ app.controller("flicker", function($http,$scope,$scope, $log, $timeout){
 
 
     $http.get(url).success(function(data){
-         $scope.photos = data.photos.photo
+       $scope.photos = data.photos.photo;
+       $scope.pages = data.photos.pages;
+
     });
-       $scope.map = {center: {latitude: 37.7994, longitude: 122.3950 }, zoom: 2 };
-       $scope.options = {scrollwheel: false};
-       $scope.coordsUpdates = 0;
-       $scope.dynamicMoveCtr = 0;
-       $scope.marker = {
-         id: 0,
-         coords: {
-           latitude: 37.7994,
-           longitude: 122.3950
-         },
-         options: { draggable: true },
-         events: {
-           dragend: function (marker, eventName, args) {
-             $log.log('marker dragend');
-             var lat = marker.getPosition().lat();
-             var lon = marker.getPosition().lng();
-             $log.log(lat);
-             $log.log(lon);
-             var url = base + api_key +'&per_page=50&page='+page+'&format=json&nojsoncallback=1&lat='+ lat +'&lon=' + lon ;
-             // var url = base + api_key +'&per_page=50&page='+page+'&format=json&nojsoncallback=1';
-             $log.log(url);
+    $scope.next = function(){
+        page++;
+        var url = base + api_key +'&per_page=50&page='+page+'&format=json&nojsoncallback=1&lat='+ lat +'&lon=' + lon ;
+        $log.log(url);
+        $http.get(url).success(function(data){
+           $scope.photos = data.photos.photo;
+           $scope.pages = data.photos.pages;
+            $('.photo').animate().scrollLeft(0);
 
+        });
+    }
+    $scope.map = {center: {latitude: 43.14351298090284, longitude: -100.84718750000002 }, zoom: 4};
+    $scope.options = {scrollwheel: false};
+    $scope.coordsUpdates = 0;
+    $scope.dynamicMoveCtr = 0;
+    $scope.marker = {
+     id: 0,
+     coords: {
+       latitude: 43.14351298090284,
+       longitude: -100.84718750000002
+     },
+     options: { draggable: true },
+     events: {
+       dragend: function (marker, eventName, args) {
+         $log.log('marker dragend');
+         var lat = marker.getPosition().lat();
+         var lon = marker.getPosition().lng();
+         $log.log(lat);
+         $log.log(lon);
+         var url = base + api_key +'&per_page=50&page='+page+'&format=json&nojsoncallback=1&lat='+ lat +'&lon=' + lon ;
+         $log.log(url);
+         $http.get(url).success(function(data){
+              $scope.photos = data.photos.photo;
+         });
 
-                 $http.get(url).success(function(data){
-                      $scope.photos = data.photos.photo
-                 });
-            // for enable draging marker from one pplace to another and some other class
-             $scope.marker.options = {
-               draggable: true,
-               labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-               labelAnchor: "100 0",
-               labelClass: "marker-labels"
-             };
-           }
-         }
-       };
+        // for enable draging marker from one pplace to another and some other class
+         $scope.marker.options = {
+           draggable: true,
+           labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+           labelAnchor: "100 0",
+           labelClass: "marker-labels"
+         };
+       }
+     }
+    };
 
-       //for checking how many times you have drag the marker
-       $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
-         if (_.isEqual(newVal, oldVal))
-           return;
-         $scope.coordsUpdates++;
-       });
+    //for checking how many times you have drag the marker
+    $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
+     if (_.isEqual(newVal, oldVal))
+       return;
+     $scope.coordsUpdates++;
+    });
 
 });
